8cf66fe402ca4e7c88c8a43da887311c05f00105
Chen Pang <chen@triplebyte.com>
2018-03-21 14:11:02 -0700

    Detect node/yarn version

--- lib/language_pack/helpers/nodebin.rb
+++ lib/language_pack/helpers/nodebin.rb
@@ -1,6 +1,8 @@
 require 'json'
+require "language_pack/shell_helpers"
 
 class LanguagePack::Helpers::Nodebin
+
   URL = "https://nodebin.herokai.com/v1/"
 
   def self.query(q)
@@ -10,20 +12,32 @@ class LanguagePack::Helpers::Nodebin
     end
   end
 
-  def self.hardcoded_node_lts
-    version = "8.9.4"
+  def self.hardcoded_node_lts(version = nil)
+    version ||= '8.9.4'
     {
       "number" => version,
       "url"    => "https://s3pository.heroku.com/node/v#{version}/node-v#{version}-linux-x64.tar.gz"
     }
   end
 
-  def self.hardcoded_yarn
-    version = "1.3.2"
+  def self.hardcoded_yarn(version = nil)
+    version ||= '1.3.2'
     {
       "number" => version,
       "url"    => "https://yarnpkg.com/downloads/#{version}/yarn-v#{version}.tar.gz"
     }
+  end
+
+  def self.detected_versions
+    return @detected_versions if defined? @detected_versions
+
+    @detected_versions = {}
+    if File.exists? 'package.json'
+      package = JSON.parse(File.read('package.json'))
+      engines = package['engines']
+      @detected_versions.merge!(engines) if !engines.empty?
+    end
+    @detected_versions
   end
 
   def self.node(q)
@@ -31,10 +45,12 @@ class LanguagePack::Helpers::Nodebin
   end
 
   def self.node_lts
-    hardcoded_node_lts # node("latest?range=6.x")
+    version = detected_versions['node']
+    hardcoded_node_lts(version) # node("latest?range=6.x")
   end
 
   def self.yarn(q)
-    hardcoded_yarn # query("yarn/linux-x64/#{q}")
+    version = detected_versions['yarn']
+    hardcoded_yarn(version) # query("yarn/linux-x64/#{q}")
   end
 end
